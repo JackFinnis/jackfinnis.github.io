b00d3871c7dedfa995c0de7fae3b7d2b605672ad
Jack Finnis <jack.finnis@icloud.com>
2024-03-29 16:59:58 +0000

    reformat load projects script

--- static/script.js
+++ static/script.js
@@ -2,33 +2,33 @@ async function load_projects() {
     var response = await fetch('/static/data/projects.json')
     var projects = await response.json()
     
-    for (project of projects) {
-        var project_box = document.createElement('a')
-        project_box.className = 'project'
+    for (var project of projects) {
         if (project.url) {
+            project_box = document.createElement('a')
             project_box.href = project.url
             project_box.target = '_blank'
+        } else {
+            project_box = document.createElement('div')
         }
+        project_box.className = 'project'
+        projects_grid.appendChild(project_box)
         
-        var img = document.createElement('img')
+        img = document.createElement('img')
         img.className = 'project_img'
         img.src = 'static/images/projects/' + project.image
         project_box.appendChild(img)
         
-        var content = document.createElement('div')
+        content = document.createElement('div')
         content.className = 'project_content'
-        
-        var h3 = document.createElement('h3')
+        project_box.appendChild(content)
+
+        h3 = document.createElement('h3')
         h3.innerHTML = project.title
         content.appendChild(h3)
-        
-        var p = document.createElement('p')
+
+        p = document.createElement('p')
         p.innerHTML = project.description
         content.appendChild(p)
-        
-        project_box.appendChild(content)
-        
-        projects_grid.appendChild(project_box)
     }
 }
 
